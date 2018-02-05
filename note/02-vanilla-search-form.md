9658fb2b5a6ace2ec926a8df9a06a7f5955274fb
gloriajun <pureainu@gmail.com>
2018-02-05 23:39:41 +0900

    검색폼 구현3-엔터를 입력하면 검색 결과가 보인다

--- note/02-vanilla-search-form.md
+++ note/02-vanilla-search-form.md
@@ -25,3 +25,33 @@ controller에 FormView를 import한 뒤에 호출한다.
 this.inputEl.addEventListener('keyup', e => this.onkeyup(e));
 ```
 
+
+###### 엔터를 입력하면 검색 결과가 보인다
+FormVeiw는 검색 결과에 대해 출력을 하지 않아도 된다.
+단지, MainController에게 알려주면 된다.
+그러면 MainController는 전달받은 결과를 ResultView에게 전해준다.
+
+이렇게 MainController에 전달할 때 사용되는 함수가 `emit` 이다.
+```javascript
+// 전달받는 쪽에서는 on 메서드를 이용하여 해당 이벤트를 정의
+FormView.setup(document.querySelector('form'))
+    .on('@submit', e => this.onSubmit(e.detail.input));
+    
+onSubmit(input) {
+    console.log(tag, 'onSubmit', input);
+}    
+    
+// 전달하는 쪽에서는 해당 이벤트를 전달
+this.emit('@submit', this.inputEl.value);
+```
+
+그리고 submit 이벤트 발생 시에는 keyup 이벤트에서 무시하도록 아래와 같이 처리해준다.
+```javascript
+FormView.bindEvents = function () {
+    this.on('submit', e => e.preventDefault());
+    this.inputEl.addEventListener('keyup', e => this.onKeyup(e));
+}
+```
+
+또한 위와 같이 체이닝으로 구현한 경우에는 `FormView`에서 this를 리턴해주어야한다.
+
--- vanilla/js/views/FormView.js
+++ vanilla/js/views/FormView.js
@@ -1,29 +1,34 @@
 import View from './View.js'
 
+const tag = '[FormView]'
 
-const tag = '[FormView]';
-
-const FormView = Object.create(View);
+const FormView = Object.create(View)
 
 FormView.setup = function (el) {
-    this.init(el);
-    this.inputEl = el.querySelector('[type=text]');
-    this.resetEl = el.querySelector('[type=reset]');
-    this.showResetBtn(false);
-    this.bindEvents();
+    this.init(el)
+    this.inputEl = el.querySelector('[type=text]')
+    this.resetEl = el.querySelector('[type=reset]')
+    this.showResetBtn(false)
+    this.bindEvents()
+
+    return this
 }
 
 // 검색 버튼 제어 함수
 FormView.showResetBtn = function (show = true) {
-    this.resetEl.style.display = show ? 'block' : 'none';
+    this.resetEl.style.display = show ? 'block' : 'none'
 }
 
 FormView.bindEvents = function () {
-    this.inputEl.addEventListener('keyup', e => this.onkeyup(e));
+    this.on('submit', e => e.preventDefault());
+    this.inputEl.addEventListener('keyup', e => this.onKeyup(e));
 }
 
-FormView.onkeyup = function () {
+FormView.onKeyup = function (e) {
+    const enter = 13; // 엔터키의 키 값
     this.showResetBtn(this.inputEl.value.length);
+    if (e.keyCode !== enter) return;
+    this.emit('@submit', { input: this.inputEl.value });
 }
 
 export default FormView;
