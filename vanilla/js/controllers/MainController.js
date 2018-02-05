ae1a5737543ad46990adfc6af7fadb12384c9e60
gloriajun <pureainu@gmail.com>
2018-02-06 00:21:11 +0900

    검색결과 구현 -검색 결과가 검색폼 아래 위치한다

--- vanilla/js/controllers/MainController.js
+++ vanilla/js/controllers/MainController.js
@@ -1,4 +1,6 @@
 import FormView from '../views/FormView.js'
+import ResultView from '../views/ResultView.js'
+import SearchModel from '../models/SearchModel.js'
 
 
 const tag = '[MainController]';
@@ -8,14 +10,28 @@ export default {
         console.log(tag, 'init()');
         FormView.setup(document.querySelector('form'))
             .on('@submit', e => this.onSubmit(e.detail.input))
-            .on('@reset', e => this.onResetForm(e))
+            .on('@reset', e => this.onResetForm(e));
+
+        ResultView.setup(document.querySelector('#search-result'))
+    },
+
+    search(query) {
+        console.log(tag, 'search()', query);
+        SearchModel.list(query).then(data => {
+            this.onSearchResult(data);
+        })
     },
 
     onSubmit(input) {
         console.log(tag, 'onSubmit()', input);
+        this.search(input);
     },
 
     onResetForm(e) {
         console.log(tag, 'onReset()');
+    },
+
+    onSearchResult(data) {
+        ResultView.render(data);
     }
 }
