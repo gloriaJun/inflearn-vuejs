a35c19076394caeca8b3cf6458c6e130fb0abc26
gloriajun <pureainu@gmail.com>
2018-02-07 01:09:06 +0900

    최근 검색어 구현 - 검색일자, 버튼 목록이 있다

--- vanilla/js/views/HistoryView.js
+++ vanilla/js/views/HistoryView.js
@@ -9,5 +9,13 @@ HistoryView.setup = function (el) {
     return this;
 }
 
+HistoryView.getKeywordResultItemHtml= function(item, no) {
+    return `<li data-keyword="${item.keyword}">
+        ${item.keyword}
+        <span class="date">${item.date}</span>
+        <button class="btn-remove"></button>
+    </li>`
+}
+
 
 export default HistoryView;
